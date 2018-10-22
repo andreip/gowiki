bc0b00a0851a6212fd0ef555aa2352d14f15bb72
Andrei Petre <p31andrei@gmail.com>
2018-10-21 20:30:29 -0700

    Make a view front page from which to navigate to existing views.

--- wiki.go
+++ wiki.go
@@ -5,21 +5,36 @@ import (
     "io/ioutil"
     "log"
     "net/http"
+    "path/filepath"
     "regexp"
+    "strings"
 )
+
+var pageDir = "data/"
+var templateDir = "tmpl/"
+var templateFilenames = []string{
+    filepath.Join(templateDir, "edit.gohtml"),
+    filepath.Join(templateDir, "view.gohtml"),
+    filepath.Join(templateDir, "view_frontpage.gohtml"),
+}
 
 type Page struct {
     Title string
     Body []byte
 }
 
+func FilenameToPagename(filename string) string {
+    // remove .txt part to get the page name
+    return strings.TrimSuffix(filename, ".txt")
+}
+
 func (p *Page) save() error {
-    filename := "data/" + p.Title + ".txt"
+    filename := filepath.Join(pageDir, p.Title + ".txt")
     return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := "data/" + title + ".txt"
+    filename := filepath.Join(pageDir, title + ".txt")
     body, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
@@ -27,11 +42,10 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-var templates = template.Must(template.ParseFiles(
-    "tmpl/edit.gohtml", "tmpl/view.gohtml"))
+var templates = template.Must(template.ParseFiles(templateFilenames...))
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    err := templates.ExecuteTemplate(w, tmpl + ".gohtml", p)
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+    err := templates.ExecuteTemplate(w, tmpl + ".gohtml", data)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
@@ -67,12 +81,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
     }
 }
 
+func viewFrontPageHandler(w http.ResponseWriter, r *http.Request) {
+    files, err := ioutil.ReadDir(pageDir)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    pages := make([]string, len(files))
+    for i, file := range files {
+        pages[i] = FilenameToPagename(file.Name())
+    }
+    renderTemplate(w, "view_frontpage", pages)
+}
+
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
+        // cannot allow anyone to edit FrontPage, even if they're not
+        // affecting it, it looks like a bug
+        if m == nil || (m != nil && m[2] == "FrontPage") {
             http.NotFound(w, r)
             return
         }
@@ -80,9 +109,23 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
     }
 }
 
+func redirectHandler(path, redirect string, code int) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != path {
+            http.NotFound(w, r)
+            return
+        }
+        http.Redirect(w, r, redirect, code)
+    }
+}
+
 func main() {
+    http.HandleFunc("/view/FrontPage", viewFrontPageHandler)
     http.HandleFunc("/view/", makeHandler(viewHandler))
     http.HandleFunc("/edit/", makeHandler(editHandler))
     http.HandleFunc("/save/", makeHandler(saveHandler))
+
+    // redirect to view/FrontPage
+    http.HandleFunc("/", redirectHandler("/", "/view/FrontPage", http.StatusFound))
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
